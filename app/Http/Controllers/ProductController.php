941ab71a8fb535c4354b543f4a4883faaefe21e0
Samuel Sabino <sabinodasilvasamuel@gmail.com>
2023-09-19 21:29:55 -0300

    fix: updating in controllers products

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class ProductController extends Controller
+{
+    public function firstproduct () {
+        return 'Olá, mundo!';
+    }
+}
